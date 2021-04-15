efeff1c9e907f9eb0de0019edc0fec16517de447
Daniel Souza <danisztls@gmail.com>
2021-04-14 21:19:36 -0300

    Review setup script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,54 @@
 #!/bin/sh
-# Script to install/uninstall Journal
+# Setup for Journal
+
+_findVim() {
+# Look if vim/nvim is installed and find the right place to install an after configuration
+    if [ "$(command -v vim)" ] || [ "$(command -v nvim)" ]; then
+        if [ "$VIMINIT" ]; then
+            vimAfter=$(printf "%s\n" "$VIMINIT" | sed "s/.*runtimepath+=~\///;s/|source.*//")
+        else
+            vimAfter="$HOME/.vim/after"
+        fi
+    fi
+
+}
 
 doInstall() {
     # Install binaries
-	bin="$HOME/.local/bin" && mkdir -p "$bin" && cp journal "$bin/"
+	binDir="$HOME/.local/bin"
+    mkdir -p "$binDir"
+    cp journal "$binDir/"
     
-    # Create config dir if not existent
-    config="$XDG_CONFIG_HOME/journal" && mkdir -p "$config"
+    # Create config dir if nonexistent
+    configDir="$XDG_CONFIG_HOME/journal"
+    mkdir -p "$configDir"
     
-    # Create default ignore file if non existent
-    [ -f "$config/ignore" ] || cp -n "ignore" "$config/" 
+    # Install default ignore file if nonexistent
+    [ -f "$configDir/ignore" ] || cp -n "ignore" "$configDir/" 
 
-    # Install syntax for vim
-    if [ "$(command -v vim)" ] || [ "$(command -v nvim)" ]; then
-        if [ "$VIMINIT" ]; then
-            vim_after=$(printf "%s\n" "$VIMINIT" | sed "s/.*runtimepath+=~\///;s/|source.*//") && vim_after="$HOME/$vim_after"
-        else
-            vim_after="$HOME/.vim/after"
-        fi
-        mkdir -p "$vim_after" && cp "syntax/markdown.vim" "$vim_after/syntax/markdown.vim"
+    # Install syntax for Vim
+    _findVim
+    if [ "$vimAfter" ]; then
+        mkdir -p "$vimAfter"
+        cp -n "syntax/journal.vim" "$vimAfter/journal.vim"
     fi
-
     printf "Setup completed!\n"
 }
 
 doUninstall() {
-    rm -rf "$bin/journal"
+    # Remove binaries
+    rm -rf "$binDir/journal"
+
+    # Leave program data
+
+    # Remove syntax for Vim
+    _findVim
+    if [ "$vimAfter" ]; then
+        rm "$vimAfter/journal.vim"
+    fi
 }
 
 case $1 in
 	install) doInstall;;
 	uninstall) doUninstall;;
 esac
-
